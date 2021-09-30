d777f64e6b268cb4cd2d8b30b27448766faac7ef
Radium Fox <eteone.longa@gmail.com>
2021-09-30 14:08:44 +0300

    make adaptive footer

--- source/js/main-nav.js
+++ source/js/main-nav.js
@@ -1,14 +1,13 @@
-const navMain = document.querySelector(".main-nav");
-const navToggle = document.querySelector(".main-nav__toggle");
-
-navMain.classList.remove("main-nav--nojs");
-
-navToggle.addEventListener("click", () => {
-  if (navMain.classList.contains("main-nav--closed")) {
-    navMain.classList.remove("main-nav--closed");
-    navMain.classList.add("main-nav--opened");
+'use strict';
+const navMain = document.querySelector('.main-nav');
+const navToggle = document.querySelector('.main-nav__toggle');
+navMain.classList.remove('main-nav--nojs');
+navToggle.addEventListener('click', () => {
+  if (navMain.classList.contains('main-nav--closed')) {
+    navMain.classList.remove('main-nav--closed');
+    navMain.classList.add('main-nav--opened');
   } else {
-    navMain.classList.add("main-nav--closed");
-    navMain.classList.remove("main-nav--opened");
+    navMain.classList.add('main-nav--closed');
+    navMain.classList.remove('main-nav--opened');
   }
 });
