03fc1e7cdca63464e8284002a6bb9ef79f4eec8c
Radium Fox <eteone.longa@gmail.com>
2021-10-03 20:44:31 +0300

    add smooth scroll

--- source/js/main-nav.js
+++ source/js/main-nav.js
@@ -11,3 +11,13 @@ navToggle.addEventListener('click', () => {
     navMain.classList.remove('main-nav--opened');
   }
 });
+
+const navLinks = navMain.querySelectorAll('a');
+
+navLinks.forEach(link => {
+  link.addEventListener('click', (evt)=> {
+    const id = evt.target.dataset.id;
+    const element = document.querySelector(`#${id}`);
+    element.scrollIntoView({block: 'center', behavior: 'smooth'});
+  });
+});
