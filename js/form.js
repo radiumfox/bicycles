a21f1a3846c34312fa4254fb89d993deca26a93a
Radium Fox <eteone.longa@gmail.com>
2021-10-05 18:17:32 +0300

    Update 2021-10-05T15:17:28.114Z

--- js/form.js
+++ js/form.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const introFormWrapper = document.querySelector('.intro__form-wrapper');
 const introNoJs = document.querySelector('.intro__nojs');
 const requestForm = introFormWrapper.querySelector('.intro__form');
@@ -10,7 +12,9 @@ introFormWrapper.classList.remove('intro__form-wrapper--nojs');
 introNoJs.classList.add('intro__nojs--hide');
 
 let isStorageSupport = true;
+// eslint-disable-next-line no-unused-vars
 let phoneStorage = '';
+// eslint-disable-next-line no-unused-vars
 let nameStorage = '';
 
 try {
@@ -43,35 +47,4 @@ requestForm.addEventListener('submit', (evt) => {
   }
   requestForm.classList.add('intro__form--hide');
   closeButton.addEventListener('click', hideSuccessMessage);
-});
-
-const navMain = document.querySelector('.main-nav');
-const navToggle = document.querySelector('.main-nav__toggle');
-const body = document.querySelector('.page-body');
-const navLinks = navMain.querySelectorAll('a');
-
-navMain.classList.remove('main-nav--nojs');
-
-navToggle.addEventListener('click', () => {
-  if (navMain.classList.contains('main-nav--closed')) {
-    navMain.classList.remove('main-nav--closed');
-    navMain.classList.add('main-nav--opened');
-    body.classList.add('page-body--no-scroll');
-  } else {
-    navMain.classList.add('main-nav--closed');
-    navMain.classList.remove('main-nav--opened');
-    body.classList.remove('page-body--no-scroll');
-  }
-});
-
-navLinks.forEach(link => {
-  link.addEventListener('click', (evt)=> {
-    link.removeAttribute('href');
-    navMain.classList.add('main-nav--closed');
-    navMain.classList.remove('main-nav--opened');
-    body.classList.remove('page-body--no-scroll');
-    const id = evt.target.dataset.id;
-    const element = document.querySelector(`#${id}`);
-    element.scrollIntoView({block: 'center', behavior: 'smooth'});
-  });
 });
