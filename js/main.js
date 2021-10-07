2dfccad7394977b0d19da3021e590f6978e4c59d
Radium Fox <eteone.longa@gmail.com>
2021-10-07 18:47:05 +0300

    Update 2021-10-07T15:47:00.820Z

--- js/main.js
+++ js/main.js
@@ -17,15 +17,14 @@ if (introFormWrapper) {
   const onPhoneInput = () => {
     if(inputPhone.value){
       if (phoneRegExp.test(inputPhone.value)){
-        introFormWrapper.classList.remove('intro__form-wrapper--disabled');
-        submitButton.removeAttribute('disabled');
+        inputPhone.setCustomValidity('');
+        inputPhone.reportValidity();
       } else {
-        introFormWrapper.classList.add('intro__form-wrapper--disabled');
-        submitButton.setAttribute('disabled', 'disabled');
+        inputPhone.setCustomValidity('В это поле нельзя вводить буквы');
+        inputPhone.reportValidity();
       }
     } else {
-      introFormWrapper.classList.remove('intro__form-wrapper--disabled');
-      submitButton.removeAttribute('disabled');
+      inputPhone.setCustomValidity('');
     }
   };
 
@@ -107,3 +106,23 @@ if (navMain) {
     });
   });
 }
+
+const DEFAULT_ZOOM = 16;
+const DEFAULT_COORDS = {
+  lat: 59.93879020329167,
+  lng: 30.32306104365654
+};
+
+const mapWrapper = document.querySelector('.page-footer__map');
+
+if (mapWrapper) {
+  const map = L.map('map')
+  .setView(DEFAULT_COORDS, DEFAULT_ZOOM);
+
+  L.tileLayer(
+    'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+    {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    },
+  ).addTo(map);
+};
